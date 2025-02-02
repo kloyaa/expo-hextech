af90aa910393ac21a3d60cf8decb38d91e5e6fe3
Niks <niks@Nikss-Mac-mini.local>
2025-02-02 14:58:57 +0800

    chore: run formatting

--- app/(auth)/request-reset-password.tsx
+++ app/(auth)/request-reset-password.tsx
@@ -1,12 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Text, View } from 'react-native';
+import React from 'react';
 
-const RequestResetPassword = () => {
-    return (
-        <View>
-            <Text>RequestResetPassword</Text>
-        </View>
-    )
-}
+const RequestResetPassword = (): JSX.Element => {
+  return (
+    <>
+      <View>
+        <Text>RequestResetPassword</Text>
+      </View>
+    </>
+  );
+};
 
 export default RequestResetPassword;
